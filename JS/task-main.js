b328ceeb295c4079233df081fa24ce008bd9ff9e
Igor <70631855+Igor-Tomilo@users.noreply.github.com>
2021-01-20 13:11:40 +0200

    completed

--- JS/task-main.js
+++ JS/task-main.js
@@ -19,7 +19,7 @@ console.log('getUsersWithEyeColor', getUsersWithEyeColor(users, 'blue')); // [о
 // // Получить массив имен пользователей по полу (поле gender).
 
 const getUsersWithGender = (users, gender) =>
-  users.filter(user => user.gender === gender);
+  users.filter(user => user.gender === gender), .map((user) => user.name);
 
 console.log('getUsersWithGender', getUsersWithGender(users, 'male')); // [ 'Moore Hensley', 'Ross Vazquez', 'Carey Barr', 'Blackburn Dotson' ]
 
